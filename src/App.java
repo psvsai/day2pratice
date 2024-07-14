c73f57137aa4c87b77ad6261907a92b92d4c9232
psvsai <pasupuletisvsm245@gmail.com>
2024-07-14 11:00:27 +0530

    hi

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Car car1 = new Car();
+        car1.Horn();
+        car1.run();
+        car1.light();
+        car1.flashlight();
+
+        Car car2 = new Car();
+        car2.Horn();
+        car2.run();
+        car2.light();
+
+        Car car3 = new Car();
+        car3.Horn();
+        car3.run();
+        car3.light();
+
     }
 }
---
+++ src/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+
+    public void Horn(){
+        System.out.println("beep");
+    }
+
+    public void run(){
+        System.out.println("Run");
+    }
+   
+    public void light(){
+        System.out.println("light");
+    }
+
+    public void flashlight(){
+        System.out.println("flashlight");
+    }
+}
